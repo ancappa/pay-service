0b28d2bc3fe9e44e6e8d745c48230cb90bcd1d2e
Annamaria Cappa <annamaria.cappa@eng.it>
2018-10-10 16:11:13 +0200

    Add files via upload

---
+++ src/main/java/it/tim/pay/common/headers/TransientHeaderName.java
@@ -0,0 +1,29 @@
+package it.tim.pay.common.headers;
+
+/**
+ * Created by alongo on 10/05/18.
+ */
+public enum TransientHeaderName {
+
+    SESSIONID("sessionID"),
+    BUSINESSID("businessID"),
+    MESSAGEID("messageID"),
+    TRANSACTIONID("transactionID"),
+    SOURCESYSTEM("sourceSystem"),
+    CHANNEL("channel"),
+    INTERACTION_DATE("interactionDate-Date"),
+    INTERACTION_TIME("interactionDate-Time"),
+    DEVICE_TYPE("deviceType"),
+	CLIENT_VERSION("clientVersion"),
+	HOST("host");
+	 
+    private String headerName;
+
+    TransientHeaderName(String headerName) {
+        this.headerName = headerName;
+    }
+
+    public String value() {
+        return headerName;
+    }
+}
