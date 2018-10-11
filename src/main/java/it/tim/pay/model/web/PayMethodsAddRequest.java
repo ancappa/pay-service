6e910d819a209fa395b54a468b11979c4fe07207
Annamaria Cappa <annamaria.cappa@eng.it>
2018-10-11 12:07:34 +0200

    Add files via upload

--- src/main/java/it/tim/pay/model/web/PayMethodsAddRequest.java
+++ src/main/java/it/tim/pay/model/web/PayMethodsAddRequest.java
@@ -9,23 +9,22 @@ import lombok.ToString;
 @Setter
 @ToString
 public class PayMethodsAddRequest {
-	
-	/*
-	@ApiModelProperty(notes = "Payment type" , required = true, dataType = "string", example = "CC")
-    private String paymentType;
-    */
-	
-	//
-	
-	@ApiModelProperty(notes = "Payment Amount Value" , required = true, dataType = "long", example = "100")
-    private long amountValue;
-	
-	@ApiModelProperty(notes = "Payment channel" , required = true, dataType = "string", example = "CC")
+	//sistemaPagamento
+	@ApiModelProperty(notes = "Payment system" , required = true, dataType = "string", example = "NETS")
+    private String paymentSystem;
+    	
+	@ApiModelProperty(notes = "Payment channel" , required = true, dataType = "string", example = "TIMMOTORIC")
     private String merId;
 	
-	@ApiModelProperty(notes = "Payment authorization date" , required = false, dataType = "string", example = "1111-AAAA")
+	@ApiModelProperty(notes = "Payment Transaction Id" , required = true, dataType = "string", example = "111")
+    private String txId;
+	
+	@ApiModelProperty(notes = "Payment authorization date" , required = true, dataType = "string", example = "1111-AAAA")
     private String txDt;
 	
 	@ApiModelProperty(notes = "Payment authorization Id" , required = true, dataType = "int", example = "0")
     private int origTranId;
+	
+	@ApiModelProperty(notes = "Payment Amount Value" , required = true, dataType = "long", example = "100")
+    private long amountValue;
 }
